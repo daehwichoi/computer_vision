180249289b4d97a5e09538ae1eb9c0f8e2905ce4
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-02-23 23:21:42 +0900

    custom resnet18 model add

---
+++ 2_image_classification_custom.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision
+from model.resnet import Resnet
+
+
+from torchsummary import summary
+
+
+# Hyper Parameter
+n_epoch = 50
+log_interval = 100
+batch_num = 128
+learning_rate = 0.001
+
+
+def train(model, epoch, train_loader, optimizer, device='cuda'):
+    model.train()
+    running_loss = 0.0
+
+    criterion = nn.CrossEntropyLoss()
+
+    # X:Img, Y:Label
+    for batch_id, (X,Y) in enumerate(train_loader):
+        X,Y = X.to(device), Y.to(device)
+        optimizer.zero_grad()
+        output = model(X)
+        loss = criterion(output,Y)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if batch_id % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_id * len(X), len(train_loader.dataset),
+                100. * batch_id / len(train_loader), running_loss/log_interval))
+            running_loss =0.0
+
+
+
+def test(model,test_loader, device='cuda'):
+    model.eval()
+    test_loss = 0.0
+    correct = 0
+    criterion =  nn.CrossEntropyLoss(reduction='sum')
+
+    with torch.no_grad():
+        for X, Y in test_loader:
+            X, Y = X.to(device), Y.to(device)
+            output = model(X)
+            loss = criterion(output, Y)
+            test_loss +=  loss.item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(Y.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+
+def main():
+    gpu_flag = torch.cuda.is_available()
+    print("GPU Check :",gpu_flag)
+    device = 'cuda' if gpu_flag else 'cpu'
+    train_flag = True
+
+    # Augmentation : Random Crop, RandomHorizontal Flip
+    # PreProcessing : Noramlization
+    train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor(),
+                                transforms.Normalize((0.4914,0.4822,0.4465),(0.2023,0.1994,0.2010))])
+    test_transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.4914,0.4822,0.4465),(0.2023,0.1994,0.2010))])
+    
+    trainset = torchvision.datasets.CIFAR10('./data/train/',transform=train_transform,train=True,download=True)
+    testset = torchvision.datasets.CIFAR10('./data/test/',transform=test_transform,train=False,download=True)
+
+    train_loader = torch.utils.data.DataLoader(trainset,batch_size=batch_num, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False)
+
+    # torchvision model load 
+    model = Resnet(num_classes=10).to(device=device)
+
+    summary(model,input_size=(3,32,32))
+
+
+    # Optimizer Select 
+    # optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    if train_flag:
+        for epoch in range(1,n_epoch+1):
+            train(model, epoch=epoch, optimizer=optimizer, train_loader=train_loader)
+            torch.save(model.state_dict(), "./training_weight/model_"+str(epoch)+".pt")
+            test(model,test_loader=test_loader)
+    else:
+        test(model,test_loader=test_loader)
+
+if __name__ == '__main__':
+    main()
